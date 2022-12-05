e262253c799c646008156360c158f822111fc41f
skiotto93 <fabio.forenza@hotmail.com>
2022-12-05 12:26:17 +0100

    Aggiunto header (da aggiungere le icon di FA)

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            links: ['HOMES', 'PAGES', 'BLOG', 'SHOP', 'EVENTS', 'ELEMENTS']
+        }
+    }
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../img/logo-img-01.png" alt="logo">
+            </div>
+            <div class="nav-header">
+                <nav>
+                    <ul>
+                        <li v-for="link in links">
+                            <a href="#"><h4>{{ link }}</h4></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    max-width: 1760px;
+    display: flex;
+    justify-content: space-between;
+    padding: 40px 0;
+    margin: auto;
+    img {
+        height: 25px;
+    }
+    ul {
+        display: flex;
+        li {
+        list-style: none;
+        padding: 0 40px;
+            a {
+                text-decoration: none;
+                color: #000;
+            }
+        }
+    }
+    
+}
+</style>
